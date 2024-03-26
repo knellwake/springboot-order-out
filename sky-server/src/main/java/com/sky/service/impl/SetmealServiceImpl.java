656d35d646fd907f291b69487a0e2df77590e091
Doug <649941598@qq.com>
2024-03-26 23:06:37 +0800

    套餐管理：新增

---
+++ sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -0,0 +1,49 @@
+package com.sky.service.impl;
+
+import com.sky.dto.SetmealDTO;
+import com.sky.entity.Setmeal;
+import com.sky.entity.SetmealDish;
+import com.sky.mapper.DishMapper;
+import com.sky.mapper.SetmealDishMapper;
+import com.sky.mapper.SetmealMapper;
+import com.sky.service.SetmealService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class SetmealServiceImpl implements SetmealService {
+
+    @Autowired
+    private SetmealMapper setmealMapper;
+
+    @Autowired
+    private SetmealDishMapper setmealDishMapper;
+
+    /**
+     * 新增套餐
+     * @param setmealDTO
+     */
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public void save(SetmealDTO setmealDTO) {
+        Setmeal setmeal = new Setmeal();
+        BeanUtils.copyProperties(setmealDTO,setmeal);
+
+        // 查询setmealId 赋值给第三方表的外键 插入后要返回一个ID传给第三方表插入
+        setmealMapper.insert(setmeal);
+        Long setmealId = setmeal.getId();
+
+        // 查询菜品信息 罗列list 根据类型查找菜品
+        List<SetmealDish> setmealDishes = setmealDTO.getSetmealDishes();
+        if(setmealDishes != null && setmealDishes.size() > 0){
+            for (SetmealDish setmealDish : setmealDishes) {
+                setmealDish.setSetmealId(setmealId);
+                setmealDishMapper.insert(setmealDish);
+            }
+        }
+    }
+}
