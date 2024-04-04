3fb2c3c0b07f2942025fc5844142895a21ebb681
Doug <649941598@qq.com>
2024-04-04 14:54:57 +0800

    工作台数据

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -12,9 +12,16 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface SetmealMapper {
+    /**
+     * 根据条件统计套餐数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
     /**
      * 根据分类ID统计套餐数量
      * @param id
